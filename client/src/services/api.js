419a29c50f184a30292ad11808ad7912a4ba6bd8
Harmanpreet-Deo <harmanpreet.deo@student.kpu.ca>
2025-04-16 21:10:03 -0700

    commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://uninest-backend.onrender.com/api', // ⬅️ your real backend URL here
+    baseURL: 'https://uninest-connect.onrender.com/api',
     withCredentials: true
 });
 
